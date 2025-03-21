ff7f98307e6f3f73ee73a558fa183ee414439f1f
phuocvinh730 <phuocvinh911@gmail.com>
2025-03-21 10:50:23 +0700

    footer.php

---
+++ footer.php
@@ -0,0 +1,31 @@
+<footer class="footer">
+      <div class="footer-container">
+        <div class="footer-logo">
+          <!--  gan hinh  -->
+          <img src="" />
+        </div>
+
+        <div class="footer-info">
+          <h3>Địa chỉ</h3>
+          <p>
+            <strong>Chi nhánh 1:</strong> 0 đường Abc phường Xyz, quận IJK TOHCM
+          </p>
+          <p>
+            <strong>Chi nhánh 2:</strong> 0 đường Abc phường Xyz, quận IJK TOHCM
+          </p>
+          <p>
+            <strong>Chi nhánh 3:</strong> 0 đường Abc phường Xyz, quận IJK TOHCM
+          </p>
+        </div>
+
+        <div class="footer-contact">
+          <h3>Thông tin liên hệ</h3>
+          <p><strong>CSKH:</strong> 0123456789 (giờ hành chính)</p>
+          <p><strong>Email:</strong> booktorestudent@gmail.com</p>
+          <p><strong>Facebook:</strong> Bookstore Student</p>
+          <p><strong>Zalo:</strong> 0123456789 (Bookstore Student)</p>
+        </div>
+      </div>
+
+      <button class="scroll-to-top" onclick="scrollToTop()">&#8593;</button>
+    </footer>
