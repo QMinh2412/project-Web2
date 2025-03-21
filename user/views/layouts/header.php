c1caa4d4f330a7d7abe8b97aafdd31e45dd740bd
QMinh2412 <luongquangminh241205@gmail.com>
2025-03-21 09:47:33 +0700

    hoàn thành giao diện header

---
+++ user/views/layouts/header.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<link rel="stylesheet" href="../../assets/css/header.css">
+<head>
+    <div class="header">
+        <div id="logo_header"><img src="../../../common/images/logo3.png" alt="Logo"></div>
+        <div class="search_box">
+            <form>
+                <input type="text" name="search_box" id="search_box" placeholder="TÌm kiếm">
+                <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </form>
+        </div>
+        <div class="cart_box">
+            <i class="fa-solid fa-cart-shopping"></i>
+            <span>Giỏ hàng</span>
+        </div>
+        <div class="account_box">
+            <div class="avatar">
+                <img src="../../assets/images/avatar_default.png" alt="avatar">
+            </div>
+            <span>Tài khoản</span>
+            <div class="sub_menu">
+                <ul>
+                    <li>Đăng nhập</li>
+                    <li>Đăng ký</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</head>
