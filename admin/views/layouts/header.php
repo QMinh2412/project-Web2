528294fe6b64077251e8d191fa230fbe3e675fed
Dinh Phuoc Hung <phuochung1311@gmail.com>
2025-03-22 20:55:37 +0700

    cap nhat

--- admin/views/layouts/header.php
+++ admin/views/layouts/header.php
@@ -1,5 +1,8 @@
-<head>
-    <div class="header">
-        <div id="logo_header"><img src="../../../common/images/logo3.png" alt="logo"></div>
+<!-- header.php -->
+<header>
+  <div class="header">
+    <div id="logo_header">
+      <img src="../common/images/logo3.png" alt="logo">
     </div>
-</head>
+  </div>
+</header>
