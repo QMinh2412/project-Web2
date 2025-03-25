dcbdcc4d94b218e972176e6e182888874d7af695
anhkhang121105 <trananhkhang121105@gmail.com>
2025-03-25 20:40:10 +0700

    update admin, i'm tired

--- admin/views/layouts/leftmenu.php
+++ admin/views/layouts/leftmenu.php
@@ -4,7 +4,9 @@
     <div class="menuitems">Danh mục sách</div>
     <div class="menuitems">Sản phẩm</div>
     <div class="menuitems">Tài khoản</div>
-    <div class="menuitems">Quản lý kho</div>
+    <div class="menuitems">Quản lý kho
+        <i class='bx bxs-chevron-down' id="down-arrow"></i>
+    </div>
     <div class="menuitems">Đơn hàng</div>
     <div class="menuitems">Đánh giá</div>
 </div>
