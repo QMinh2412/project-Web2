7dfa8c29c265aeb09d03093dcedd6d65e631dc7f
QMinh2412 <luongquangminh241205@gmail.com>
2025-04-07 21:35:31 +0700

    tạm thời link qua lại đc giữa trang home và trang sản phẩm

--- user/views/layouts/main_layout.php
+++ user/views/layouts/main_layout.php
@@ -13,7 +13,7 @@
     <div style="background-color: #f1f1f1;">
         <?php 
             echo $main_content; 
-            include __DIR__ . '/../product/product.php'; // Include the main content dynamically
+            // include __DIR__ . '/../product/product.php'; // Include the main content dynamically
         ?>
     </div>
     <?php
