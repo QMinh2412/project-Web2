a485b13ce93a4df250f0ac851ca771405382e5fc
anhkhang121105 <trananhkhang121105@gmail.com>
2025-04-22 10:44:09 +0700

    edit creating product in admin

--- admin/views/layouts/leftmenu.php
+++ admin/views/layouts/leftmenu.php
@@ -1,6 +1,11 @@
 <script src="./assets/js/leftmenu.js"></script>
 
 <div id="left-menu">
+
+    <div class="menuitems" id="admin-menu-icon">
+        <i class='bx bx-menu'></i>  
+    </div>
+    
     <a href="?page=dashboard&action=index">
         <div class="menuitems">
             <i class='bx bx-stats'></i>
