10574f2377a513f6dad4887514b503b723f5c45e
anhkhang121105 <trananhkhang121105@gmail.com>
2025-05-15 15:27:02 +0700

    update DATABASE and update provider deleting feature in admin

--- admin/views/provider/create.php
+++ admin/views/provider/create.php
@@ -2,8 +2,8 @@
     <div class="admin-header" id="provider-header">
         <h2 id="provider-header-title">Thêm nhà cung cấp</h2>
         <form id="create-provider-form" method="POST" action="?page=provider&action=create">
-            <label class="provider-create-label" for="provider-name">Tên:</label><br>
-            <input class="provider-create-text" type="text" name="provider_name" id="provider-name-input" placeholder="Tên" required>
+            <label class="provider-create-label" for="provider-name">Tên nhà cung cấp:</label><br>
+            <input class="provider-create-text" type="text" name="provider_name" id="provider-name-input" placeholder="Tên nhà cung cấp" required>
             <label class="provider-create-label" for="provider-address">Địa chỉ:</label><br>
             <input class="provider-create-text" type="text" name="provider_address" id="provider-address-input" placeholder="Địa chỉ">
             <label class="provider-create-label" for="provider-email">Email:</label><br>
